746acdba6783855b31578141cc4dfd4b09201860
Dan-123 <Dan-123@users.noreply.github.com>
2018-02-22 16:38:40 -0800

    Updated to PEP8 Standards

--- readDicomDoseCube.py
+++ readDicomDoseCube.py
@@ -8,38 +8,36 @@ This is a temporary script file.
 import numpy as np
 import dicom
 
-def ReadDicomDoseCube(fname):
-    
-    
-    dinfo = dicom.read_file(fname)
-    
-    x0 = dinfo.ImagePositionPatient[0]
-    y0 = dinfo.ImagePositionPatient[1]
-    z0 = dinfo.ImagePositionPatient[2]
 
-    T = dinfo.ImageOrientationPatient
-    nx = dinfo.Width
-    ny = dinfo.Height
-    doseScale = dinfo.DoseGridScaling
-    unitStr = dinfo.DoseUnits
-    typeStr = dinfo.DoseType
-    dx = dinfo.PixelSpacing[0]
-    dy = dinfo.PixelSpacing[1]
-    zoff = dinfo.GridFrameOffsetVector
+def read_dicom_dose_cube(file_name):
+    dicom_info = dicom.read_file(file_name)
+
+    x0 = dicom_info.ImagePositionPatient[0]
+    y0 = dicom_info.ImagePositionPatient[1]
+    z0 = dicom_info.ImagePositionPatient[2]
+
+    T = dicom_info.ImageOrientationPatient
+    nx = dicom_info.Width
+    ny = dicom_info.Height
+    doseScale = dicom_info.DoseGridScaling
+    unitStr = dicom_info.DoseUnits
+    typeStr = dicom_info.DoseType
+    dx = dicom_info.PixelSpacing[0]
+    dy = dicom_info.PixelSpacing[1]
+    zoff = dicom_info.GridFrameOffsetVector
     dz = np.diff(zoff)
     errDz = np.linalg.norm(np.diff(dz))
-    
+
     if errDz < 0.1:
         dz = dz[0]
     else:
         print(['z-spacing not uniform! - ' + str(errDz)])
-        
+        print("z-spacing not uniform! - {0}", str(errDz))
+
     x = np.linspace(x0, dx, xn)
     y = np.linspace(y0, dy, yn)
     for i in zoff:
         z[i] = z0 + zoff[i]
-#    z = z0 + zoff
-    
+    #    z = z0 + zoff
+
     print(z)
-    
-    
